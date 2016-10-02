d025730c40458070a158109c029e8e1ab4f1dc5a
darden1 <darden066@gmail.com>
2016-10-02 17:05:39 +0900

    predictionPriceBot

--- examples/predictionPriceBot.py
+++ examples/predictionPriceBot.py
@@ -17,7 +17,7 @@ ppList=[]
 tommorrwPricePrediction=[]
 
 #---Prediction price and back test
-for coinIndex in range(0,1): #---range(len(coins)):
+for coinIndex in range(len(coins)):
     pp = PredictionPrice(currentPair=basicCoin+"_"+coins[coinIndex], workingDirPath=workingDirPath,
                          gmailAddress=myGmailAddress, gmailAddressPassword=myGmailAddressPassword,
                          backTestOptNumFeatureMin=backTestOptParams[coinIndex][0], backTestOptNumFeatureMax=backTestOptParams[coinIndex][1],
