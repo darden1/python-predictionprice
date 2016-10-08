cbc79a49ec61f59a8751a747a21da21217a51df2
darden1 <darden066@gmail.com>
2016-10-08 21:00:23 +0900

    not use gevent

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         url              = "https://github.com/darden1/python-predictionprice/",
         keywords         = "",
         packages         = find_packages(),
-        install_requires = ["numpy","pandas","matplotlib","scikit-learn","apscheduler","gevent","poloniex"],
+        install_requires = ["numpy","pandas","matplotlib","scikit-learn","apscheduler","poloniex"],
         dependency_links = ["https://codeload.github.com/s4w3d0ff/python-poloniex/zip/master#egg=poloniex"],
         zip_safe=False
         )
