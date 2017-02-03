35b7362a691ba0021aa4560371ed8e4f33e35569
Tylor Darden <darden066@gmail.com>
2017-02-03 23:25:35 +0900

    Update setup.py
    Modified install_requires and dependency_links.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         url              = "https://github.com/darden1/python-predictionprice/",
         keywords         = "",
         packages         = find_packages(),
-        install_requires = ["numpy","pandas","matplotlib","scikit-learn","apscheduler","poloniex"],
-        dependency_links = ["https://codeload.github.com/s4w3d0ff/python-poloniex/zip/master#egg=poloniex"],
+        install_requires = ["numpy","pandas","matplotlib","scikit-learn","apscheduler","poloniex==0.2.2"],
+        dependency_links = ["git+https://git@github.com/darden1/python-poloniex.git@master#egg=poloniex-0.2.2"],
         zip_safe=False
         )
